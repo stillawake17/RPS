6f412d47963d3ce4e29ad9a146d65f0a07ca83ab
whydidIchoosephysics <ionelvlad123@gmail.com>
2023-01-05 20:36:23 +0000

    initial if statement to check if it's a tie works

--- script.js
+++ script.js
@@ -1,9 +1,63 @@
 let choices = ["r", "p", "s"];
 let userInput = prompt("Choose r, p or s");
-// let computerInput
+// let computerInput = Math.floor(Math.random() * 3); //Picks a random number from 0,1,2
 
-// let userWins
-// let userLoses
-// let ties
+let computerString = "r";
 
-Math.floor(Math.random() * 3); //Picks a random number from 0,1,2
+// if (computerInput == "0") {
+//     computerString = "r";
+//  } else if (computerInput == "1") {
+//         computerString = "p";
+//      } else (computerInput == "2") 
+//             computerString = "s";
+        
+    
+
+
+let userWins = 0;
+let userLoses = 0;
+let ties = 0;
+
+
+if (userInput == null || userInput == "") {
+    text = "User cancelled the prompt.";
+    console.log(text);
+  } else 
+    if (userInput === computerString) {
+            text = "It's a tie";
+            console.log(text);
+            ties++;
+            console.log(ties);
+        } else {
+            switch (computerInput) {
+                case 0:
+                  text = "Off";
+                  break;
+                case 1:
+                 text = "On";
+                  break;
+                default:
+                  text = "No value found";
+              }
+        }
+  
+
+
+
+//   if (userInput == null || userInput == "" || userInput !== "r" || userInput !== "s" || userInput !== "p") {
+//     text = "You need to enter R P or S to play this game.";
+//   } else {
+//     text =  ;
+//   }
+
+// let x = "0";
+// switch (x) {
+//   case 0:
+//     text = "Off";
+//     break;
+//   case 1:
+//    text = "On";
+//     break;
+//   default:
+//     text = "No value found";
+// }
